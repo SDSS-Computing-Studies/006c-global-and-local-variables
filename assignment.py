878eb8b7a23a8520d9a5442c81c108893c884327
Chris Yang <cyang@deltalearns.ca>
2021-10-25 12:04:46 -0700

    Update for 2021

--- assignment.py
+++ assignment.py
@@ -20,30 +20,18 @@ def instructions():
     # Modified:
     return None
 
-def getParams(shape):
-    # Will create a list of questions to be asked depending on the shape.
-    # These will be asked so that the user can enter in appropriate values
-    # input parameter: string 
-    # output parameter: return a list containing the prompts for each shape:
-    # example: ["Enter the radius:","Enter the slant height:","Enter the height:"]
-    prompts
 
-    return prompts
-
-def getInputs(questions):
-    # Will prompt the user for inputs for the shape they.
-    # These will be asked so that the user can enter in appropriate values
-    # It will turn all the input data into a list
-    # input parameter: list containing the prompts/questions
-    # output parameter: return a list containing all the measurements of the shape
-    measurements
-    
-    return measurements
 
 def main():
-    # main block of code that will run your program and control program flow
-    # You will need to include a while loop to keep repeating the commands until
-    # the user chooses to exit
+    """
+    main block of code that will run your program and control program flow
+    You will need to include a while loop to keep repeating the commands until
+    the user chooses to exit
+    """
     title()
+    while True:
+        # keep giving options to choose menu options until they choose to exit
+        pass
 
-main()
+if __name__ == "__main__":
+    main()
